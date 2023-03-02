550343adf93afe63bbd97f5f9c3daa6bae3b5789
Micci11 <miccibenner@gmail.com>
2023-03-02 12:58:31 -0500

    changes

--- script.js
+++ script.js
@@ -12,16 +12,49 @@ var numericChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 function generatePassword() {
 
 var numberofChars = window.prompt("How many characters do you want in your password?");
-var isLowercase = window.confirm("Do you want to include lowercase?");
 
+if(numberofChars < 8 || numberofChars > 128) {
+  alert("bad character length")
+  return "Error"
+}
+
+var isLowercase = window.confirm("Do you want to include lowercase?");
+var isUppercase = window.confirm("Do you want to inclue uppercase?");
+var isNumeric = window.confirm("Do you want to include numbers?");
+var isSpecial = window.confirm("Do you want to include special characters?");
+var possibleChars = [];
+
+if (isLowercase) {
+  possibleChars = possibleChars.concat(lowercaseChars);
+}
+
+if (isUppercase) {
+  possibleChars = possibleChars.concat(uppercaseChars);
+}
+
+if (isNumeric) {
+  possibleChars = possibleChars.concat(numericChars);
+}
+
+if (isSpecial) {
+  possibleChars = possibleChars.concat(specialChars);
+}
+
+var generatedPasswords = "";
 
 for (let i = 0; i < numberofChars; i++) {
-  if (isLowercase) {
-    var randomLowercase = Math.floor(Math.random() * lowercaseChars.length);
-    var lowercaseOutcome = lowercaseChars[randomLowercase];
-    console.log(lowercaseOutcome);
-  }
+  var randomLetterIndex = Math.floor(Math.random()* possibleChars.length);
+  var randomLetter = possibleChars[randomLetterIndex];
+  generatedPasswords += randomLetter
+
+  // if (isLowercase) {
+    // var randomLowercase = Math.floor(Math.random() * lowercaseChars.length);
+    // var lowercaseOutcome = lowercaseChars[randomLowercase];
+    // console.log(lowercaseOutcome);
+  // }
+
 }
+return generatedPasswords
 }
 
 
